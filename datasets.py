c2cb2225b12e11de17582d1f8b611ecb82eca14c
Koyomi12 <38738998+Koyomi12@users.noreply.github.com>
2025-01-29 04:31:28 +0100

    load datasets

---
+++ datasets.py
@@ -0,0 +1,58 @@
+from pathlib import Path
+
+import torch
+import torchvision.datasets as datasets
+import torchvision.transforms.v2 as transforms
+from torch.utils.data import DataLoader
+
+train_transform = transforms.Compose(
+    [
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.RandomVerticalFlip(p=0.5),
+        # transforms.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5)),
+        transforms.ToImage(),
+        transforms.ToDtype(torch.float32, scale=True),
+        transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),
+    ]
+)
+
+test_transform = transforms.Compose(
+    [
+        transforms.ToImage(),
+        transforms.ToDtype(torch.float32, scale=True),
+        transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),
+    ]
+)
+
+train_dataset = datasets.ImageFolder(
+    Path(
+        "/home/niklas/Documents/dev/uni/bees/bee-classifier/data/cropped/50x50_1/train"
+    ),
+    train_transform,
+)
+validation_dataset = datasets.ImageFolder(
+    Path(
+        "/home/niklas/Documents/dev/uni/bees/bee-classifier/data/cropped/50x50_1/validation"
+    ),
+    test_transform,
+)
+test_dataset = datasets.ImageFolder(
+    Path(
+        "/home/niklas/Documents/dev/uni/bees/bee-classifier/data/cropped/50x50_1/test"
+    ),
+    test_transform,
+)
+
+batch_size = 32
+
+train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+validation_dataloader = DataLoader(
+    validation_dataset,
+    batch_size=batch_size,
+)
+test_dataloader = DataLoader(test_dataset, batch_size=120)
+
+# for X, y in test_dataloader:
+#     print(f"Shape of X [N, C, H, W]: {X.shape}")
+#     print(f"Shape of y: {y.shape} {y.dtype}")
+#     break
